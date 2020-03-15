04c0f408de3b775a22fb99a43b8004a731cef8ee
Nurul Aisyah <aisynurul99@gmail.com>
2020-03-15 11:56:43 +0700

    Updates

---
+++ precache-manifest.6e63af9ca12a8472ceceacdf24bbec01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a146d4a37e4441b993e39bb9a5c47dfb",
+    "url": "/simple-chat-room/index.html"
+  },
+  {
+    "revision": "36531e303e5f98a2c06f",
+    "url": "/simple-chat-room/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6f057abd7f15dd2be529",
+    "url": "/simple-chat-room/static/js/2.07fb2529.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/simple-chat-room/static/js/2.07fb2529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36531e303e5f98a2c06f",
+    "url": "/simple-chat-room/static/js/main.38625981.chunk.js"
+  },
+  {
+    "revision": "6f0961659456f117244e",
+    "url": "/simple-chat-room/static/js/runtime-main.2f852540.js"
+  }
+]);
